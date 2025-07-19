6604673f6d4062431f116a8425522876adb6ff2f
najmul905 <nh4808563@gmail.com>
2025-07-19 23:51:26 +0600

    ReverseString problem solving use to for loop

--- Beginner Level/ReverseString.js
+++ Beginner Level/ReverseString.js
@@ -1,4 +1,15 @@
+// using Reverse method
 const ReverseString=(data)=>{
     return data.split("").reverse().join("")
 }
 console.log(ReverseString("Hello World"))
+
+// using for loop
+const ReverseStringForLoop = (data) => {
+    let reversed="";
+    for(let i=data.length-1; i>=0; i--){
+        reversed=reversed + data[i];
+    }
+    return reversed;
+}
+console.log(ReverseStringForLoop("Hello world with for"))
